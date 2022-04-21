2c09b18c40ce9ee5e76949f6fc85ce12c16b8afe
Shane Grim <grim8020@gmail.com>
2022-04-21 17:24:37 -0400

    Added tip percent and scroll bar

--- BasicTipCalculator/BasicTipCalculator/ContentView.swift
+++ BasicTipCalculator/BasicTipCalculator/ContentView.swift
@@ -8,9 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var total = ""
+    @State var tipPercent = 15.0
+    
     var body: some View {
-        Text("Hello, world!")
+        
+        VStack {
+            
+            Text("Tip Calculator")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(Color.green)
+                .padding()
+            
+            
+            HStack {
+                
+                Text("$")
+                    .padding(0.5)
+                TextField("Amount", text: $total)
+                    .padding()
+
+            }
+            .padding(.all)
+            
+            HStack {
+                
+                Slider(value: $tipPercent, in: 1...30, step: 1.0)
+                Text("\(Int(tipPercent))%")
+            }
             .padding()
+            
+            Text("Tip Amount: $\(Double(total)! * (tipPercent/100.0))")
+            
+        }
     }
 }
 
