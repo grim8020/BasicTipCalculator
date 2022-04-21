00de9d786734dda362acd2a32a02e1cd0124d139
Shane Grim <grim8020@gmail.com>
2022-04-21 15:50:10 -0400

    Added Xcode

---
+++ BasicTipCalculator/BasicTipCalculator/BasicTipCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicTipCalculatorApp.swift
+//  BasicTipCalculator
+//
+//  Created by Shane Grim on 4/21/22.
+//
+
+import SwiftUI
+
+@main
+struct BasicTipCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
